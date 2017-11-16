ddd780e9cfdd914ef8ac0f05694efb40668530dd
Taras Tymchiy <Taras.Tymchiy@eleks.com>
2017-11-17 01:08:03 +0200

    implemented get/set functions

--- src/ts-object-path.ts
+++ src/ts-object-path.ts
@@ -1,6 +1,8 @@
 import { ObjPathProxy } from './obj-path-proxy';
 export * from './obj-path-proxy';
 
+export type ObjProxyArg<TRoot, T> = ObjPathProxy<TRoot, T> | ((p: ObjPathProxy<TRoot, TRoot>)=> ObjPathProxy<TRoot, T>);
+
 const pathSymbol = Symbol('Object path');
 
 export function createProxy<T>(path: PropertyKey[] = []): ObjPathProxy<T, T> {
@@ -21,11 +23,28 @@ export function createProxy<T>(path: PropertyKey[] = []): ObjPathProxy<T, T> {
   return proxy as any as ObjPathProxy<T, T>;
 }
 
-export function getPath<TRoot, T>(obj: ObjPathProxy<TRoot, T>): PropertyKey[] {
-  return (obj as any)[pathSymbol];
+export function getPath<TRoot, T>(proxy: ObjProxyArg<TRoot, T>): PropertyKey[] {
+  if (typeof proxy === 'function') {
+    proxy = proxy(createProxy<TRoot>())
+  }
+  return (proxy as any)[pathSymbol];
 }
 
 export function isProxy<TRoot, T>(value: any): value is ObjPathProxy<TRoot, T> {
   return value && typeof value === 'object' && !!getPath<TRoot, T>(value as ObjPathProxy<TRoot, T>);
+}
+
+export function get<TRoot, T>(object: TRoot, proxy: ObjProxyArg<TRoot, T>, defaultValue: T|null|undefined = undefined) {
+  return getPath(proxy).reduce((o, key) => o && o[key] || defaultValue, object as any) as T;
+}
+
+export function set<TRoot, T>(object: TRoot, proxy: ObjProxyArg<TRoot, T>, value: T): void {
+  getPath(proxy).reduce((o: any, key, index, keys) => {
+    if (index < keys.length - 1) {
+      o[key] = o[key] || (typeof keys[index + 1] === 'number' ? [] : {});
+      return o[key];
+    }
+    o[key] = value;
+  }, object);
 }
 
--- test/ts-object-path.test.ts
+++ test/ts-object-path.test.ts
@@ -1,16 +1,16 @@
-import { createProxy, isProxy, getPath } from "../src/ts-object-path"
+import { createProxy, isProxy, getPath, get, set } from "../src/ts-object-path"
 import {} from 'jest';
 
 interface ITest {
   one: number;
-  two: string;
-  three: INestedTest;
-  four: INestedTest[];
+  two?: string;
+  three?: INestedTest;
+  four?: INestedTest[];
 }
 
 interface INestedTest {
   firrst: number;
-  second: string;
+  second?: string;
 }
 
 describe("createProxy test", () => {
@@ -51,5 +51,86 @@ describe("isProxy test", () => {
     expect(isProxy(false)).toBeFalsy();
     expect(isProxy(7)).toBeFalsy();
     expect(isProxy('test')).toBeFalsy();
+  })
+})
+
+
+describe("getValue test", () => {
+  it("Works with proxy", () => {
+    const p = createProxy<ITest>();
+    const o: ITest = { one: 4 };
+    const v = get(o, p.one);
+    expect(v).toEqual(o.one);
+  })
+  it("Works with callback", () => {
+    const v = get({ one: 4 }, p=> p.one);
+    expect(v).toEqual(4);
+  })
+  it("Works with deep props", () => {
+    let o: ITest;
+    expect(get<ITest, number>(o, p=> p.three.firrst)).toBeUndefined();
+
+    o = { one: 5, three: { firrst: 4 } };
+    expect(get<ITest, number>(o, p=> p.three.firrst)).toEqual(o.three.firrst);
+  })
+  it("Works with arrays", () => {
+    const o: ITest = { one: 5, four: [null, { firrst: 4 }] };
+    expect(get<ITest, number>(o, p=> p.four[0].firrst)).toBeUndefined();
+    expect(get<ITest, number>(o, p=> p.four[1].firrst)).toEqual(4);
+  })
+  it("Works with symbols", () => {
+    const symb = Symbol('test');
+    const o = { regularProp: { [symb]: 333 } };
+    const v = get(o, p=> p.regularProp[symb]);
+    expect(v).toEqual(333);
+  })
+  it("returns default value", () => {
+    const o: ITest = { one: 4 };
+    const v = get(o, p=> p.three.second, 'default' as string);
+    expect(v).toEqual('default');
+  })
+
+})
+
+describe("setValue test", () => {
+  it("Works with proxy", () => {
+    const p = createProxy<ITest>();
+    const o: ITest = { one: 4 };
+    set(o, p.one, 333);
+    expect(o.one).toEqual(333);
+  })
+  it("Works with callback", () => {
+    const o: ITest = { one: 4 };
+    set(o, p=> p.one, 333);
+    expect(o.one).toEqual(333);
+  })
+  it("Works with deep props", () => {
+    let o: ITest = { one: 5, three: { firrst: 4 } };
+    set(o, p=> p.three.firrst, 777);
+    expect(o.three.firrst).toEqual(777);
+  })
+  it("Works with arrays", () => {
+    const o: ITest = { one: 5, four: [null, { firrst: 4 }] };
+    set(o, p=> p.four[1].firrst, 666);
+    expect(o.four[1].firrst).toEqual(666);
+  })
+
+  it("Works with symbols", () => {
+    const symb = Symbol('test');
+    const o = { regularProp: { [symb]: 333 } };
+    set(o, p=> p.regularProp[symb], 555);
+    expect(o.regularProp[symb]).toEqual(555);
+  })
+
+  it("Creates nested objects", () => {
+    const o: ITest = { one: 5 };
+    set(o, p=> p.three.firrst, 777);
+    expect(o.three.firrst).toEqual(777);
+  })
+  it("Creates nested arrays", () => {
+    const o: ITest = { one: 5 };
+    set(o, p=> p.four[1].firrst, 666);
+    expect(o.four[1].firrst).toEqual(666);
+    expect(o.four).toHaveLength(2);
   })
 })
